61d98db8c67a0fcbd2e78c334000887d8c63e442
SpritersBlock <signupdump1@gmail.com>
2019-12-12 20:45:37 +1300

    Added Android Test Support, Edited Player Prefab

--- Endless Runner/Assets/Scripts/PlayerJump.cs
+++ Endless Runner/Assets/Scripts/PlayerJump.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour
 {
     [Header("Jump Numbers")]
-    public float jumpHeight;
+    public float jumpForce; //For debug purposes, this is just a singular force applied to the rigidbody.
 
     [Header("Components")] //Unity-specific components, most of which are set in Start().
     Rigidbody2D rb;
@@ -18,6 +18,14 @@ public class PlayerJump : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.touchCount > 0) //If there's any touch on the screen...
+        {
+            Touch touch = Input.GetTouch(0); //Only one touch is needed because the only control is "tap."
+
+            if (touch.phase == TouchPhase.Start)
+            {
+                rb.AddForce(jumpForce, ForceMode.Impulse);
+            }
+        }
     }
 }
