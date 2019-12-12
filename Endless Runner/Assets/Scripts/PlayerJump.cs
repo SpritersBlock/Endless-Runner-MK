e53585c44acccd668b086250fe94e4f6096aa6bd
SpritersBlock <signupdump1@gmail.com>
2019-12-12 21:10:11 +1300

    Player can only Jump when Grounded

--- Endless Runner/Assets/Scripts/PlayerJump.cs
+++ Endless Runner/Assets/Scripts/PlayerJump.cs
@@ -9,10 +9,12 @@ public class PlayerJump : MonoBehaviour
 
     [Header("Collision Info")]
     public bool isGrounded; //Is the player on the ground?
+    public float groundRayDist = 1.15f; //How long the raycast determining groundedness is from the center of the player
 
     [Header("Components")] //Unity-specific components, most of which are set in Start()
     Rigidbody2D rb;
     Animator anim;
+    public LayerMask groundLayer;
 
     void Start()
     {
@@ -21,15 +23,24 @@ public class PlayerJump : MonoBehaviour
 
     void Update()
     {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundRayDist, groundLayer);
+
         if (Input.touchCount > 0) //If there's any touch on the screen...
         {
             Touch touch = Input.GetTouch(0); //Only one touch is needed because the only control is "tap."
 
             if (touch.phase == TouchPhase.Began) //
             {
-                //[If Player is grounded]
-                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); //Apply force for the jump
+                if (isGrounded){ //Player can only jump when grounded
+                    rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); //Apply force for the jump.
+                }
+                
             }
         }
+    }
+
+    private void OnDrawGizmos(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundRayDist);
     }
 }
