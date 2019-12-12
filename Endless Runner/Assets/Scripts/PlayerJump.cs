e897761f1226ffec23a4c02cad6fd1ecd2eb6a3c
SpritersBlock <signupdump1@gmail.com>
2019-12-12 20:32:14 +1300

    Added PlayerJump.cs

---
+++ Endless Runner/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [Header("Jump Numbers")]
+    public float jumpHeight;
+
+    [Header("Components")] //Unity-specific components, most of which are set in Start().
+    Rigidbody2D rb;
+    Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
