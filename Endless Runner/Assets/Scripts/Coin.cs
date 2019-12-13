d714d573a278103ec23159bd5da401a92742ead4
SpritersBlock <signupdump1@gmail.com>
2019-12-13 17:51:52 +1300

    Player can Touch and Destroy Coins

---
+++ Endless Runner/Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : CollectableItem
+{
+    public ParticleSystem collectPFX;
+
+    public override void BeCollected()
+    {
+        Instantiate(collectPFX, transform.position, Quaternion.identity);
+    }
+}
---
+++ Endless Runner/Assets/Scripts/CollectableItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CollectableItem : MonoBehaviour
+{
+    public abstract void BeCollected();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("test");
+            BeCollected();
+            Destroy(gameObject);
+        }
+    }
+}
