47ea7129ce53fa669e3da57618e7d2230a75dbb3
SpritersBlock <signupdump1@gmail.com>
2019-12-13 22:50:50 +1300

    Added in Footstep Function, Added Dust to Run Cycle

---
+++ Endless Runner/Assets/Scripts/Animation/AnimPFX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimPFX : MonoBehaviour
+{
+    //This is an all-purpose script I like to use to trigger particle systems via animation events.
+
+    [SerializeField] ParticleSystem PFXToTrigger;
+
+    public void PlayParticleSystem()
+    {
+        PFXToTrigger.Play();
+    }
+}
---
+++ Endless Runner/Assets/Scripts/Animation/PlayerFootsteps.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    [SerializeField] ParticleSystem dustPFX;
+    public PlayerJump playerJump;
+
+    public void Footstep()
+    {
+        if (playerJump.isGrounded)
+        {
+            //Play Sound Here
+            dustPFX.Play();
+        }
+    }
+}
