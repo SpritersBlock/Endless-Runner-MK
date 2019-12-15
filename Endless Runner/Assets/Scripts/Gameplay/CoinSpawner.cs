32865e662f76aeba82764285df39ceb38e7533a4
SpritersBlock <signupdump1@gmail.com>
2019-12-15 19:40:25 +1300

    Terrain now Keeps Generating!
    It needs a bit of clean-up and nothing spawns on top of platforms but functionally I think the assignment is complete right now. The game definitely still needs a lot of polish though so I'll see how much I can get down before the deadline.

--- Endless Runner/Assets/Scripts/Gameplay/CoinSpawner.cs
+++ Endless Runner/Assets/Scripts/Gameplay/CoinSpawner.cs
@@ -6,6 +6,8 @@ public class CoinSpawner : MonoBehaviour
 {
     public float distanceBetweenCoins = 1.5f; //How much space is between coins in a group?
     public int[] coinsPerGroup; //The number of coins that can spawn at once, all should be odd
+    public float heightVar = 2; //Coins can spawn higher than normal by a random amount, this is that value
+    public float maxNumberOfLanesUp = 2; //How many lanes coins can be spawned up; should be kept somewhat low in case they spawn on platforms
 
     private void Start()
     {
@@ -15,15 +17,19 @@ public class CoinSpawner : MonoBehaviour
 
     void InstantiateGroupOfCoins()
     {
+        transform.position += new Vector3(0, heightVar * Random.Range(0,maxNumberOfLanesUp));
+
+
         int numberOfCoinsInGroup = coinsPerGroup[Random.Range(0, coinsPerGroup.Length)]; //Randomly select how many coins show up in a group
         int numberOfCoinsOnSide = Mathf.FloorToInt(numberOfCoinsInGroup / 2); //Calculates how many coins will spawn on either side of the center coin, which is our base. We use FloorToInt because we're dividing odd numbers
         GameObject coinClone = transform.GetChild(0).gameObject; //Getting a coin clone from child
+
 
         bool doubleCoins = (Random.value > 0.5f); //Adds a random chance for coin groups to be doubled
 
         if (doubleCoins)
         {
-            Instantiate(coinClone, transform.position + new Vector3(0, 2), Quaternion.identity, transform); //Spawns a coin above the center coin
+            Instantiate(coinClone, transform.position + new Vector3(0, heightVar), Quaternion.identity, transform); //Spawns a coin above the center coin
         }
         
         for (int i = 1; i < numberOfCoinsOnSide; i++)
@@ -31,7 +37,7 @@ public class CoinSpawner : MonoBehaviour
             Instantiate(coinClone, transform.position + new Vector3(distanceBetweenCoins * i, 0), Quaternion.identity, transform); //Spawn half of the coin group on the right side of the center coin. i = 1 because we don't want coins spawning in the center
             if (doubleCoins)
             {
-                Instantiate(coinClone, transform.position + new Vector3(distanceBetweenCoins * i, 2), Quaternion.identity, transform); //Spawn a double of this coin up above
+                Instantiate(coinClone, transform.position + new Vector3(distanceBetweenCoins * i, heightVar), Quaternion.identity, transform); //Spawn a double of this coin up above
             }
         }
         for (int i = 1; i < numberOfCoinsOnSide; i++)
@@ -39,7 +45,7 @@ public class CoinSpawner : MonoBehaviour
             Instantiate(coinClone, transform.position + new Vector3(-distanceBetweenCoins * i, 0), Quaternion.identity, transform); //Spawn half of the coin group on the left side of the center coin. i = 1 because we don't want coins spawning in the center
             if (doubleCoins)
             {
-                Instantiate(coinClone, transform.position + new Vector3(-distanceBetweenCoins * i, 2), Quaternion.identity, transform); //Spawn a double of this coin up above
+                Instantiate(coinClone, transform.position + new Vector3(-distanceBetweenCoins * i, heightVar), Quaternion.identity, transform); //Spawn a double of this coin up above
             }
         }
     }
--- Endless Runner/Assets/Scripts/Systems/SpawnManager.cs
+++ Endless Runner/Assets/Scripts/Systems/SpawnManager.cs
@@ -9,9 +9,6 @@ public class SpawnManager : MonoBehaviour
     public GameObject ground;
     float groundLength; //How long the ground tile is
 
-    [Header("Collectables")] //Collectables the player can pick up
-    public GameObject coin;
-
     [Header("Platforms")] //Platforms the player can run on
     public GameObject platform;
 
@@ -22,30 +19,32 @@ public class SpawnManager : MonoBehaviour
     public GameObject cloud;
 
     [Header("Spawn Variables")]
-    public Vector3[] gameSpawnPositions; //The different positions gameplay objects can spawn at
-    public Vector3[] bgSpawnPositions;
-    public int numberOfTilesSpawned; //How many instances of the ground tile do we want loaded?
     public GameObject[] gameplaySpawnableObjects; //The GAMEPLAY objects that can spawn
+    public Vector3[] bgSpawnPositions; //For this project, this is just the heights that clouds spawn at. Worth differentiating from the above spawn positions
+    public int numberOfTilesSpawned; //How many instances of the ground tile do we want loaded?
 
     private void Start()
     {
         groundLength = ground.GetComponent<SpriteRenderer>().size.x; //Get the length of the ground prefab so we can build in even units
         InitialGroundSpawn();
+        StartCoroutine("SpawnFGElements"); //Make sure the foreground elements are spawning
         StartCoroutine("SpawnBGElements"); //Make sure the background elements are spawning
     }
 
-    public void SpawnObjectType(string name) //This way, we can type in a string of an object we want to spawn, and it will be instantiated
-    {
-        GameObject objectClone = Array.Find(gameplaySpawnableObjects, GameObject => GameObject.name == name);
-        if (objectClone == null) //If there's no object with that name in the array...
-        {
-            Debug.LogWarning("Game Object: " + name + " not found!"); //...Let people know
-            return;
-        }
-        Instantiate(objectClone, gameSpawnPositions[UnityEngine.Random.Range(0, gameSpawnPositions.Length)], Quaternion.identity); //Spawn that object we found, put it on a random y axis out of the ones we chose, and let the objects themselves do the rest
-    }
+    //The below section of code has been made redundant by the Coroutine "SpawnFGElements" but I still think it's need so I'm keeping it here for you to look at.
 
-    void InitialGroundSpawn()
+    //public void SpawnObjectType(string name) //This way, we can type in a string of an object we want to spawn, and it will be instantiated
+    //{
+    //    GameObject objectClone = Array.Find(gameplaySpawnableObjects, GameObject => GameObject.name == name);
+    //    if (objectClone == null) //If there's no object with that name in the array...
+    //    {
+    //        Debug.LogWarning("Game Object: " + name + " not found!"); //...Let people know
+    //        return;
+    //    }
+    //    Instantiate(objectClone, gameSpawnPositions[UnityEngine.Random.Range(0, gameSpawnPositions.Length)], Quaternion.identity); //Spawn that object we found, put it on a random y axis out of the ones we chose, and let the objects themselves do the rest
+    //}
+
+    void InitialGroundSpawn() //We have CONTINUOUS ground spawning, but there's still a bunch of ground that needs to be spawned right away. That's this function
     {
         GameObject initialGround = FindObjectOfType<GroundScroller>().gameObject; //The initial ground object that the rest of the ground builds from
 
@@ -61,6 +60,18 @@ public class SpawnManager : MonoBehaviour
         {
             GameObject groundClone = Instantiate(ground, groundTransform.position + new Vector3(groundLength * numberOfTilesSpawned, 0), Quaternion.identity, gameObject.transform);
         }
+    }
+
+    IEnumerator SpawnFGElements()
+    {
+        while (GameManager.instance.gameActive)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(GameManager.instance.gameSpeed / 5, GameManager.instance.gameSpeed / 2)); //Wait for a fraction of the game speed
+            GameObject instantiatedObject = gameplaySpawnableObjects[UnityEngine.Random.Range(0, gameplaySpawnableObjects.Length)]; //This just makes the next line a lot shorter
+            Instantiate(instantiatedObject, instantiatedObject.transform.position, Quaternion.identity); //Spawn a gameplay object
+        }
+
+        yield return null;
     }
 
     IEnumerator SpawnBGElements()
